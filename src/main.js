01df794c890f5b8c91e03ae36c039de2ae364a26
John Runyard <jrunyard3ft@gmail.com>
2023-01-27 23:50:11 -0600

    Initial commit.

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import '@shoelace-style/shoelace/dist/themes/light.css';
+import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path';
+
+setBasePath('https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.0.0/dist/');
+
+import App from "./App.vue";
+import router from "./router";
+
+import "./assets/main.css";
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+
+app.mount("#app");
---
+++ src/views/Playground.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="about">
+    <h1>QR code generator</h1>
+
+    <sl-input
+      maxlength="255"
+      clearable
+      label="Value"
+      v-model="qrCode"
+    ></sl-input>
+
+    <sl-qr-code :value="qrCode"></sl-qr-code>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import "@shoelace-style/shoelace/dist/components/qr-code/qr-code.js";
+import "@shoelace-style/shoelace/dist/components/input/input.js";
+
+const qrCode = ref();
+</script>
+
+<style>
+.about {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
+
+.about > *:first-child {
+  margin-top: auto;
+}
+
+.about > *:last-child {
+  margin-bottom: auto;
+}
+
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+
+sl-input {
+  margin: var(--sl-spacing-large) 0;
+}
+</style>
